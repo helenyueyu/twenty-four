0181dbc1eab4ab7381ee9dc3a86e9bb1a4a78483
tokyosuite <helenyueyu@gmail.com>
2019-01-16 17:31:52 -0800

    Updates

---
+++ precache-manifest.a917f18d00250012bb7c2686428da7e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00175ede7d61d1c67310",
+    "url": "/twenty-four/static/css/main.87e007b8.chunk.css"
+  },
+  {
+    "revision": "00175ede7d61d1c67310",
+    "url": "/twenty-four/static/js/main.00175ede.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/twenty-four/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4802575157a055ab2afd",
+    "url": "/twenty-four/static/js/runtime~main.48025751.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twenty-four/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "425ee777dd18eff168973e0669d65f0e",
+    "url": "/twenty-four/index.html"
+  }
+];
