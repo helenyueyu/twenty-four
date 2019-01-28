454d4c368f0f5d2a05c2455e74f2a2e0a5c6eb44
tokyosuite <helenyueyu@gmail.com>
2019-01-28 13:56:20 -0800

    Added operator buttons.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import './App.css';
 
 class App extends Component {
   state = {
-    images: null
+    cards: null
   }
   componentDidMount() {
-    fetch('https://deckofcardsapi.com/api/deck/2faaps93vetq/draw/?count=4')
+    fetch('https://deckofcardsapi.com/api/deck/b2ri8ncygn7c/draw/?count=4')
       .then(res => res.json())
       .then(data => this.setState({
-        images: data.cards
+        cards: data.cards
       }))
+  }
+  handleClick = (e) => {
+    console.log()
   }
   render() {
     return (
-      <div className="App">
-        <h1>24</h1>
-        {this.state.images && this.state.images.map((x,idx) => <img src={x.image} key={idx}/>)}
+      <div style={{padding: '5rem'}}>
+      <h1>24</h1>
+      <p><b>Instructions:</b> The object of the game is to use exactly the four cards below to create the number 24 using any of the four operations (+, -, x, /). The order in which the numbers are executed is <b>left-to-right</b> and NOT <b>PEMDAS</b>. If you cannot find a combination, press NEXT.</p>
+      <div className="App" style={{display: 'flex', flexDirection: 'row'}}>
+        {this.state.cards && this.state.cards.map((x,idx) => <img src={x.image} key={idx} value={x.value} onClick={this.handleClick}/>)}
+
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <button>+</button>
+          <button>-</button>
+          <button>x</button>
+          <button>÷</button>
+        </div>
+      </div>
       </div>
     );
   }
